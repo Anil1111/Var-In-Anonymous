11ee071b28a7b9c9cff1762ba9371ffb1ecc17c8
anilsri.04@Gmail.com <anilsri.04@Gmail.com>
2019-04-28 11:36:46 +0530

    Var In Anonymous

--- UseVarAnonymous/Program.cs
+++ UseVarAnonymous/Program.cs
@@ -10,6 +10,22 @@ namespace UseVarAnonymous
     {
         static void Main(string[] args)
         {
+            //Example1 
+            var v = new { Amount = 101, Message = "     Hello World , The First Program Written By Sir dennis ritchie" };
+            Console.WriteLine("Example1       ");
+            Console.WriteLine(v.Amount + v.Message);
+
+
+           // Example2
+            string[] Arr = { "Shiv", "Raju", "Koirala", "Anil", "Amitabh" };
+            var o = from x in Arr where x.Length > 4 select new { Len = x.Length, value = x };
+            Console.WriteLine("Example2       ");
+            foreach (var x in o)
+            {
+                Console.WriteLine(x.ToString());
+            }
+
+            Console.ReadLine();
         }
     }
 }
